46382334b4e5f6d7870b20abbedaa34efacf8778
tamsamani <tamsmoha@gmail.com>
2021-06-19 02:17:59 +0100

    2 files modified

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 
+// Gon
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex justify-center align-center">
       <h3>Hello Pintax! it's me.</h3>
+      <p>So, let's START Now</p>
     </div>
   );
 }
